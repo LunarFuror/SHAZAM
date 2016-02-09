98b9048aa2ba8442efa9bd77c1f7b1029989a2df
Grayson Lorenz <lunar_furor@hotmail.com>
2016-02-08 22:57:25 -0600

    Create Register.java

---
+++ Register.java
@@ -0,0 +1,51 @@
+
+public class Register {
+	byte rb1, rb2, rb3;
+	
+	public Register(){
+		rb1=0x0;
+		rb2=0x0;
+		rb3=0x0;
+	}
+	
+	public Register(byte nb1,byte nb2,byte nb3){
+		rb1 = nb1;
+		rb2 = nb2;
+		rb3 = nb3;
+	}
+
+	public byte getRb1() {
+		return rb1;
+	}
+
+	public void setRb1(byte rb1) {
+		this.rb1 = rb1;
+	}
+
+	public byte getRb2() {
+		return rb2;
+	}
+
+	public void setRb2(byte rb2) {
+		this.rb2 = rb2;
+	}
+
+	public byte getRb3() {
+		return rb3;
+	}
+
+	public void setRb3(byte rb3) {
+		this.rb3 = rb3;
+	}
+
+	public byte[] getByteArray(){
+		byte[] output = {rb1, rb2, rb3};
+		return output;
+	}
+	
+	public String ToString(){
+		String output = "";
+		output = Integer.toHexString(rb1) + Integer.toHexString(rb2) + Integer.toHexString(rb3);
+		return output;
+	}
+}
