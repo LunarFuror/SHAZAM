30c767398a0f3a9b6afa6baefb4162c5ed99d2a6
Grayson Lorenz <lunar_furor@hotmail.com>
2016-04-04 13:04:27 -0500

    added a print out to ADR

--- ShazamUI.java
+++ ShazamUI.java
@@ -686,6 +686,7 @@ public class ShazamUI {
 					case (byte)0x7: //ADR push the combined level and address onto the stack.
 						t.parseString(Integer.toHexString(t.getMemoryValue()+1));
 						dataMemory[t.getRow()][t.getColumn()].parseString(Integer.toHexString(ir.getIB2()) + ir.getAddress());
+						writer.print("DATA(" + t.ToString() + ") <-- " + dataMemory[t.getRow()][t.getColumn()].ToString().toUpperCase() + " ");
 						break;
 				}
 				writer.print("\r\n");
